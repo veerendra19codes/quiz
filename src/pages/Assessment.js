f538bf753879ef2e62bc52bc6b320cf8d89a4e5a
Yash <yashkalia4215@gmail.com>
2023-10-04 19:31:08 +0530

    first commit

---
+++ src/pages/Assessment.js
@@ -0,0 +1,302 @@
+import React from 'react';
+
+
+export const Assessment = () => {
+
+
+    return (
+        <div className="App">
+            <div className='page-header'>
+                <h1>Assessment</h1>
+                <h3>Answer the following 25 questions and explore your own career</h3>
+            </div>
+
+
+        <div className="card">
+            <h1>1. What subjects do you enjoy the most in school?</h1>
+            <div className='answers'>
+                <button >a Science and Mathematics</button>
+                <button >b Literature and Arts</button>
+                <button >c Social Sciences and History</button>
+                <button >d Physical Education and Sports</button>
+            </div>
+        </div>
+
+        <div className="card">
+            <h1>2. What hobbies or activities do you find most engaging in your free time?</h1>
+            <div className='answers'>
+                <button>a Reading, writing, or drawing</button>
+                <button>b Playing musical instruments or singing</button>
+                <button>c Volunteering or helping others</button>
+                <button>d Playing sports or engaging in physical activities</button>
+            </div>
+        </div>
+
+        <div className="card">
+            <h1>3. Which of these qualities do you think best describes you?</h1>
+            <div className='answers'>
+                <button>a Analytical and logical</button>
+                <button>b Creative and imaginative</button>
+                <button>c Empathetic and compassionate</button>
+                <button>d Energetic and competitive</button>
+            </div>
+        </div>
+
+        <div className="card">
+            <h1>4. What type of work environment do you prefer?</h1>
+            <div className='answers'>
+                <button>a Quiet and organized</button>
+                <button>b Creative and expressive</button>
+                <button>c Collaborative and supportive</button>
+                <button>d Dynamic and fast-paced</button>
+            </div>
+        </div>
+
+        <div className="card">
+            <h1>5. What kind of problems do you enjoy solving?</h1>
+            <div className='answers'>
+                <button>a Math or science problems</button>
+                <button>b Artistic or design-related problems</button>
+                <button>c Social or interpersonal problems</button>
+                <button>d Competitive or strategic problems</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>6. Which of the following goals excites you the most?</h1>
+            <div className='answers'>
+                <button>a Making new scientific discoveries</button>
+                <button>b Creating original artworks or designs</button>
+                <button>c Making a positive impact on people's lives</button>
+                <button>d Achieving athletic excellence</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>7. What skills or talents do others often compliment you on?</h1>
+            <div className='answers'>
+                <button>a Problem-solving and logical thinking</button>
+                <button>b Creativity and artistic abilities</button>
+                <button>c Listening and communication skills</button>
+                <button>d Physical agility and coordination</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>8. What kind of books, movies, or TV shows do you enjoy the most?</h1>
+            <div className='answers'>
+                <button>a Science fiction or documentaries</button>
+                <button>b Fantasy or imaginative stories</button>
+                <button>c Dramas or real-life stories</button>
+                <button>d Sports or action-packed stories</button>
+            </div>
+        </div>
+
+        <div className="card">
+            <h1>9. When working on a group project, which role do you naturally assume?</h1>
+            <div className='answers'>
+                <button>a The researcher or data analyzer</button>
+                <button>b The creative thinker or idea generator</button>
+                <button>c The mediator or peacemaker</button>
+                <button>d The leader or coordinator</button>
+            </div>
+        </div>
+                
+        <div className="card">
+
+            <h1>10. If you could choose any profession without worrying about money or practicality, what would it be?</h1>
+            <div className='answers'>
+                <button>a Scientist or researcher</button>
+                <button>b Artist or designer</button>
+                <button>c Counselor or social worker</button>
+                <button>d Athlete or coach</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>11. What is your preferred working style?</h1>
+            <div className='answers'>
+                <button>a Structured and organized</button>
+                <button>b Flexible and spontaneous</button>
+                <button>c Collaborative and team-oriented</button>
+                <button>d Independent and self-driven</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>12. What type of challenges do you find most motivating?</h1>
+            <div className='answers'>
+                <button>a Intellectual challenges</button>
+                <button>b Creative challenges</button>
+                <button>c Humanitarian challenges</button>
+                <button>d Physical challenges</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>13. How do you handle stress and pressure?</h1>
+            <div className='answers'>
+                <button>a Analyze the situation and find solutions</button>
+                <button>b Express your feelings through art or creativity</button>
+                <button>c Talk to friends or seek emotional support</button>
+                <button>d Engage in physical activities or sports</button>
+            </div>
+        </div>
+
+    <div className="card"> 
+        <h1>14. Which school subjects do you excel in without much effort?</h1>
+        <div className='answers'>
+            <button>a Math or physics</button>
+            <button>b Art or music</button>
+            <button>c Social studies or psychology</button>
+            <button>d Physical education or sports</button>
+        </div>
+    </div>
+
+        <div className="card">
+
+            <h1>15. What kind of lifestyle do you envision for yourself in the future?</h1>
+            <div className='answers'>
+                <button>a Stable and financially secure</button>
+                <button>b Artistic and expressive</button>
+                <button>c Making a difference in others' lives</button>
+                <button>d Active and adventurous</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>16. How do you prefer to express yourself?</h1>
+            <div className='answers'>
+                <button>a Through logical arguments and analysis</button>
+                <button>b Through creative works like writing, art, or music</button>
+                <button>c Through empathy and understanding others' emotions</button>
+                <button>d Through physical activities and body language</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>17. What type of books or articles do you enjoy reading?</h1>
+            <div className='answers'>
+                <button>a Scientific journals or non-fiction books</button>
+                <button>b Fiction novels or poetry</button>
+                <button>c Self-help books or articles about personal development</button>
+                <button>d Sports magazines or articles about physical fitness</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>18. How do you approach new and unfamiliar situations?</h1>
+            <div className='answers'>
+                <button>a Analyze the situation and gather information</button>
+                <button>b Embrace the unknown and see it as an opportunity for creativity</button>
+                <button>c Seek guidance from others and collaborate</button>
+                <button>d Dive in headfirst and learn by doing</button>
+            </div>
+        </div>
+
+
+        <div className="card">
+
+            <h1>19. How do you handle failure or setbacks?</h1>
+            <div className='answers'>
+                <button>a Analyze what went wrong and strategize for the future</button>
+                <button>b Use it as inspiration for creative expression</button>
+                <button>c Seek emotional support and advice from others</button>
+                <button>d Channel the energy into physical activities to cope</button>
+            </div>
+        </div>
+
+
+        <div className="card">
+
+            <h1>20. What kind of projects or tasks make you lose track of time when you're working on them?</h1>
+            <div className='answers'>
+                <button>a Researching or analyzing data</button>
+                <button>b Creating art, music, or writing</button>
+                <button>c Helping others or volunteering</button>
+                <button>d Engaging in sports or physical activities</button>
+            </div>
+        </div>
+
+
+        <div className="card">
+
+            <h1>21. What values are most important to you in a workplace?</h1>
+            <div className='answers'>
+                <button>a Innovation and efficiency</button>
+                <button>b Creativity and freedom of expression</button>
+                <button>c Compassion and teamwork</button>
+                <button>d Competition and achievement</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>22. What role do you naturally assume in a social gathering or a party?</h1>
+            <div className='answers'>
+                <button>a Engage in intellectual discussions or debates</button>
+                <button>b Showcase your creative talents, like singing or drawing</button>
+                <button>c Listen attentively and offer support to friends</button>
+                <button>d Initiate and participate in playful or competitive activities</button>
+            </div>
+        </div>
+
+        <div className="card">
+
+            <h1>23. What kind of topics or issues are you most passionate about?</h1>
+            <div className='answers'>
+                <button>a Scientific advancements and technology</button>
+                <button>b Art and culture</button>
+                <button>c Social justice and equality</button>
+                <button>d Sports and physical fitness</button>
+            </div>
+        </div>
+
+
+        <div className="card">
+
+            <h1>24. What type of feedback motivates you the most?</h1>
+            <div className='answers'>
+                <button>a Constructive criticism that helps you improve</button>
+                <button>b Appreciation for your creative efforts</button>
+                <button>c Recognition for your empathy and kindness</button>
+                <button>d Recognition for your achievements and competitiveness</button>
+            </div>
+        </div>
+
+
+        <div className="card">
+
+            <h1>25. What do you enjoy learning about in your free time?</h1>
+            <div className='answers'>
+                <button>a Scientific theories or technological advancements</button>
+                <button>b Art history, literature, or creative writing</button>
+                <button>c Psychology, sociology, or human behavior</button>
+                <button>d Sports techniques, strategies, or athletes' biographies</button>
+            </div>
+        </div>
+
+<br />
+
+        <button className="submit" >Submit Form</button>
+        <p className='results'>Your results</p>
+
+        <h1>Number of A selected</h1>
+        <h1>Number of B selected</h1>
+        <h1>Number of C selected</h1>
+        <h1>Number of D selected</h1>
+</div>
+
+         
+        
+    );
+};
